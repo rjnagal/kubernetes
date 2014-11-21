c2117128f301e3bbd86f685de55ea8bd1dc0e985
Vishnu Kannan <vishnuk@google.com>
2014-11-21 21:39:27 +0000

    Dockerfile for statscollector. As of now all the services reside in a single Pod.

--- pkg/scaler/core.go
+++ pkg/scaler/core.go
@@ -17,7 +17,7 @@ var argHousekeepingTick = flag.Duration("housekeeping", 10*time.Minute, "Houseke
 var argThreshold = flag.Uint("cluster_threshold", 90, "Percentage of cluster resource usage beyond which the cluster size will be increased.")
 
 // TODO(vishh): Consider replacing minute/hour/day with intent - aggresive/moderate/conservative.
-var argScalingPolicy = flag.String("cluster_policy", "hour", "Cluster nodes will be scaled based on usage for the last minute, hour or day. Choose between 'minute' (aggresive), 'hour' (moderate) and 'day' (conservative).")
+var argScalingPolicy = flag.String("cluster_scaling_policy", "hour", "Cluster nodes will be scaled based on usage for the last minute, hour or day. Choose between 'minute' (aggresive), 'hour' (moderate) and 'day' (conservative).")
 
 var argActuatorHostPort = flag.String("actuator_hostport", "localhost:8080", "Actuator Host:Port.")
 
